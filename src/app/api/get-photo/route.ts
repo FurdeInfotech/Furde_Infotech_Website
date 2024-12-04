c8a798434acd91a99b9e0fb573955f24341cff21
Adil0710 <padil2246@gmail.com>
2024-12-04 15:20:17 +0530

    keys and bg

--- src/app/api/get-photo/route.ts
+++ src/app/api/get-photo/route.ts
@@ -1,32 +1,38 @@
 import dbConnect from "@/lib/dbConnect";
 import GalleryModel from "@/models/Gallery";
 
+export async function GET() {
+  try {
+    await dbConnect();
 
-export async function GET () {
-    try {
-        await dbConnect()
+    const gallery = await GalleryModel.find().sort({ createdAt: -1 });
+    console.log("Fetched gallery data:", gallery);
 
-        const gallery = await GalleryModel.find().sort({createdAt: -1})
-
-        
     return new Response(
-        JSON.stringify({
-          success: true,
-          gallery,
-        }),
-        { status: 200 }
-      );
-
-
-    } catch (error) {
-        console.error("Error fetching gallery images:", error);
-        return new Response(
-          JSON.stringify({
-            success: false,
-            message: "An error occurred while fetching gallery images",
-          }),
-          { status: 500 }
-        );
+      JSON.stringify({
+        success: true,
+        gallery,
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
-    }
-    
+    );
+  } catch (error) {
+    console.error("Error fetching gallery images:", error);
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message: "An error occurred while fetching gallery images",
+      }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+}
