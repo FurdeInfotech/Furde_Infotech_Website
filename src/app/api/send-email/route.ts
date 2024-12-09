28205f86b2e9bf02887aa6edcb0f816633683104
Adil0710 <padil2246@gmail.com>
2024-12-09 14:41:12 +0530

    checking email

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -209,7 +209,7 @@ export async function POST(req: Request) {
 
     // Attempt to send the email
     const { data, error } = await resend.emails.send({
-      from: "New Application Received <onboarding@resend.dev>",
+      from: "New Application Received <info@furdeinfotech.com>",
       to: "info@furdeinfotech.com",
       subject: `Application Received for ${designation}`,
       replyTo: email,
